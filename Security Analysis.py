4637e59bf3c54cff1a17b406d608d58869f84259
Ehsan Estaji <ehsan.estaji@uni.lu>
2020-04-05 23:00:09 +0200

    Update Security Analysis.py

--- Security Analysis.py
+++ Security Analysis.py
@@ -1,6 +1,8 @@
 # First Version
+import copy
 Swap=set()
 WrongDigit=set()
+WrongList=set()
 
 
 # k is the length of PIN and num is the Number
@@ -26,26 +28,54 @@ def swapPositions(list, pos1, pos2):
 
 # This function generate all possible swapping error list of a PIN
 def swapErrors(List):
-    print(List)
     for j in range(len(List)-1):
         Swap.add(swapPositions(List,j,j+1))
         Swap.add(swapPositions(List,0,0))
+    #print("<<<<<===============================================>>>>>")
+    #print("Here is the list of all PIN's with sequential errors ")
+    #print(Swap)
     return Swap
 
 
 #This function generate all possible errors for just one wrong digit
 def WrongDigitErrors(List,i):
+    new_list = copy.deepcopy(List)
     for j in range(10):
-        List[i]=j
-        WrongDigit.add(tuple(List))
+        new_list[i]=j
+        WrongDigit.add(tuple(new_list))
     return WrongDigit
+
+#print(WrongDigitErrors([1,2,3,4],0))
+
+
+
+
 
 def WrongDigitErrorFull(List):
     for j in range(len(List)):
+        #print(j,"====",WrongDigit, len(WrongDigit),type(WrongDigit))
         WrongDigitErrors(List,j)
+        #WrongDigit.add(WrongDigitErrors(List,j))
+    #print("<<<<<===============================================>>>>>")
+    #print("Here is the list of all PIN's with just one wrong digit ")
+    #print(WrongDigit)
     return WrongDigit
 
-print(len(WrongDigitErrorFull([1,1,0,1])))
+
+def PinSpan(List):
+    return swapErrors(List).union(WrongDigitErrorFull(List))
+
+
+
+print(PinSpan([1,2,3]))
+
+
+#def PinErrorList(List):
+ #   return WrongDigitErrorFull(List).union(swapErrors(List))
+
+
+
+
 
 
 
@@ -67,10 +97,10 @@ for num in range(1000):
 #print(type(list(num3[0])))
 
 
-print("This is the list of all errors for 3 digits PIN")
+#print("This is the list of all errors for 3 digits PIN")
 #for j in range(10):
 #    print(j, swapErrors(list(num3[j])))
-j=998
+#j=998
 #print(num3[j],"==========",swapErrors(list(num3[j])))
 
 #
